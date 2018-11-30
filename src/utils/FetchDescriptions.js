fd4823cd63bcdf404938c82c26b663a807d37427
aSondergaard <andrewsondergaard@gmail.com>
2018-11-30 12:59:49 +0100

    Feat: Add feeding

--- src/utils/FetchDescriptions.js
+++ src/utils/FetchDescriptions.js
@@ -3,7 +3,7 @@ export default () => {
 		{
 			Title: 'Description title',
 			Location: 'Placed in area C',
-			FeedingTime: null,
+			FeedingTime: "Feeding: 10:00 and 12:00",
 			Body: "<h3>Breeding like domestic animals</h3><p>The alpaca is a domestic animal and therefore does not as such have a natural distribution. It is believed to be domesticated from the wild tribal vicuña in Peru several thousand years ago. Since then, humans have spread the alpaca to large parts of South America, especially in central and southern Andes. The alpaca lives mostly in the heights, and it can be found up to 4800 meters. In the 1980s, alpacas began to export to other countries and continents, and therefore it is now available in both the United States, Australia and across parts of Europe.</p><h3>The Alpaca has the finest wool</h3><p>In South America there are four different species of lamb animals: Alpaca, Vicuña, Guanako and Lama. Of these species, alpaca and lama are livestock, while vicuña and guanacoen are wildlife. All lamb animals are close to each other genetic and can be cross-linked with each other.The Alpaca looks like its ancestor Vicuña in both appearance and behavior, but it differs from being a little bigger and has a very long colored coat. The Vicuña has only one color, while in alpacas there are about 22 different colors, ranging from white to brown to black, and both colored and alpaca with multicolored coat.The Alpaca is especially famous for its very fine wool, which can grow 50 cm long, unless it is cut by regularly. Alpacauld is both finer and warmer than ordinary wool, and therefore alpaca wool is also a lot more expensive to buy.</p><h3>A wool rumors</h3><p>The alpaca is a ruminant animal like a cow. And like the cow, the alpaca has more stomachs to help with the decomposition of food. However, the alpaca has only three stomachs where one cow has four. The alpaca is only born of grass, whereas llamas and guanacos live from both grass and leaves.</p><h3>Violent mating play</h3><p>Like vicuña, alpacas live in flocks. Internally in the flock there will be a hierarchy, and the flock is led by a dominant male. The males are fighting for the right to lead the flock. For this, they use their specially developed fighting teeth that can be over 3 cm long.Alpacaans have induced ovulation like lions. This means that the females must either be paired repeatedly or over a longer period of time to provoke ovulation. Before mating, the male may seem very violent as he will repeatedly try to dominate the female. To dominate her and show how strong he is, he will force her to lie down. He does so by biting her in her legs and throwing herself on her back and throat. Alpaca is mating as it is the mating that stimulates ovulation. A pairing can easily take over an hour and therefore it is more energy-saving to lay down.</p>",
 			DescriptionType: 'Animal',
 			DescriptionId: 1
@@ -11,7 +11,7 @@ export default () => {
 		{
 			Title: 'Description title',
 			Location: 'Placed in area C',
-			FeedingTime: null,
+			FeedingTime: "Feeding: 12:30 and 13:00",
 			Body: '<p>lorem ipsum sit dolor amet bao bao banana bao</p>',
 			DescriptionType: 'Animal',
 			DescriptionId: 2
@@ -19,7 +19,7 @@ export default () => {
 		{
 			Title: 'Description title',
 			Location: 'Placed in area C',
-			FeedingTime: null,
+			FeedingTime: "Feeding: 09:00 and 11:00",
 			Body: '<p>lorem ipsum sit dolor amet bao bao banana bao</p>',
 			DescriptionType: 'Accommodation',
 			DescriptionId: 3
@@ -27,7 +27,7 @@ export default () => {
 		{
 			Title: 'Description title',
 			Location: 'Placed in area C',
-			FeedingTime: null,
+			FeedingTime: "Feeding: 10:00 and 12:00",
 			Body: '<p>lorem ipsum sit dolor amet bao bao banana bao</p>',
 			DescriptionType: 'Shop',
 			DescriptionId: 4
@@ -35,7 +35,7 @@ export default () => {
 		{
 			Title: 'Description title',
 			Location: 'Placed in area C',
-			FeedingTime: null,
+			FeedingTime: "Feeding: 10:00 and 12:00",
 			Body: '<p>lorem ipsum sit dolor amet bao bao banana bao</p>',
 			DescriptionType: 'Playground',
 			DescriptionId: 5
