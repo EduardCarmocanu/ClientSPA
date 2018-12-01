c798e510ecde3cf14fb801da597497a27dd4069d
Carmocanu Eduard <carmocanueduard@gmail.com>
2018-12-01 21:21:05 +0100

    Feat: Fixed image sources

--- src/components/Loadingscreen/Loadingscreen.vue
+++ src/components/Loadingscreen/Loadingscreen.vue
@@ -2,10 +2,10 @@
 	<div class="loadscreen-container" :class="!showLoadingScreen ? 'loadscreen-container--hide' : null">
         <div class="loadbox">
             <div class="loadscreen-box">
-                <img src="src/assets/img/icons/logo@2x.png" class="load-image">
+                <img src="@/assets/img/icons/logo@2x.png" class="load-image">
             </div>
             <div class="icon-box">
-                <img src="src/assets/img/icons/temploadlogo.png" class="icon-image">
+                <img src="@/assets/img/icons/temploadlogo.png" class="icon-image">
             </div>
             <div class="loadscreen-footer">
                 <p>Powered by givskud</p>
